7e846aa6a326679f689ce45bad4f93af06523b41
Debashish Sahu <debashish.sahu@gmail.com>
2019-06-10 12:45:59 -0400

    Update A85_KeyBoard_HID.ino

--- Arduino/A85_KeyBoard_HID/A85_KeyBoard_HID.ino
+++ Arduino/A85_KeyBoard_HID/A85_KeyBoard_HID.ino
@@ -13,30 +13,31 @@ void fnSendKeyStroke(uint8_t data_in){
   // https://github.com/adafruit/Adafruit-Trinket-USB/blob/master/TrinketKeyboard/TrinketKeyboard.h
   switch(data_in){
     case 0:
-     TrinketHidCombo.pressKey(KEYCODE_LEFT_CONTROL, KEYCODE_C);
+     TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_CONTROL, KEYCODE_C);
      break;
     case 1:
-     TrinketHidCombo.typeChar(KEYCODE_HOME);
+     TrinketHidCombo.pressKey(0, KEYCODE_HOME);
      break;
     case 2:
-     TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_GUI, KEYCODE_MOD_LEFT_SHIFT, KEYCODE_S);
+     TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_GUI | KEYCODE_MOD_RIGHT_SHIFT, KEYCODE_S);
      break;
     case 3:
      TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_GUI, KEYCODE_D);
+     delay(20);
      TrinketHidCombo.pressKey(0, 0);
      TrinketHidCombo.pressMultimediaKey(MMKEY_MUTE);
      break;
     case 4:
-     TrinketHidCombo.pressKey(KEYCODE_LEFT_CONTROL, KEYCODE_V);
+     TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_CONTROL, KEYCODE_V);
      break;
     case 5:
-     TrinketHidCombo.typeChar(KEYCODE_END);
+     TrinketHidCombo.pressKey(0, KEYCODE_END);
      break;
     case 6:
-     TrinketHidCombo.pressKey(KEYCODE_LEFT_CONTROL, KEYCODE_L);
+     TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_GUI, KEYCODE_L);
      break;
     case 7:
-     TrinketHidCombo.pressKey(KEYCODE_LEFT_CONTROL, KEYCODE_MOD_LEFT_ALT, KEYCODE_DELETE);
+     TrinketHidCombo.pressKey(KEYCODE_MOD_LEFT_CONTROL | KEYCODE_MOD_RIGHT_ALT, KEYCODE_DELETE);
      break;
   }
   TrinketHidCombo.pressKey(0, 0);
@@ -52,11 +53,11 @@ void loop() {
   unsigned int currentMillis = millis();
   TrinketHidCombo.poll();
   
-  //Limit button read to 20 times a second
+  //Limit button read to 10 times a second
   //Fast enough for most buttons 
   //but this way you don't have a dimmer output because it's blanked during button read
   //a read takes 460us t 16Mhz Arduino and normal I2C speed.
-  if(currentMillis - buttonMillis >= 50){
+  if(currentMillis - buttonMillis >= 100){
     buttonMillis = currentMillis;
     pcf20.setByte(B11111111);
     uint8_t inputStates = pcf20.getByte();
